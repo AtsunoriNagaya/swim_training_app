c15e29831d99dc0af66a060048955db992ddfc6f
Atsunori Nagaya <c230056@ed.nagoya-cu.ac.jp>
2025-04-03 07:29:47 +0900

    add pages

---
+++ app/privacy/page.tsx
@@ -0,0 +1,10 @@
+const PrivacyPage = () => {
+  return (
+    <div>
+      <h1>Privacy Policy</h1>
+      <p>This is the privacy policy page.</p>
+    </div>
+  );
+};
+
+export default PrivacyPage;
